2127bf8be66ef007de05f91b8e3b2e3ee5dd2608
yangxiaoge <364804051@qq.com>
2016-11-21 09:58:16 +0800

    litepal CRUD操作

--- app/src/main/java/com/yjn/retrofitdemo/litepal/LitePal_Activity.java
+++ app/src/main/java/com/yjn/retrofitdemo/litepal/LitePal_Activity.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.yjn.retrofitdemo.R;
-import com.yjn.retrofitdemo.litepal.models.Album;
 import com.yjn.retrofitdemo.litepal.models.Song;
 
 import org.litepal.LitePal;
@@ -27,24 +26,38 @@ public class LitePal_Activity extends AppCompatActivity {
         // 根据 litepal.xml中 mapping, dbname等配置,创建数据库
         SQLiteDatabase db = LitePal.getDatabase();
 
-//        Album album = new Album();
-//        album.setName("我在南京");
-//        album.setPrice(10.09f);
-//        album.setCover("1,2,3,4,5".getBytes());
-//        album.save();
-//
-//        Song song1 = new Song();
-//        song1.setName("my love");
-//        song1.setDuration(320);
-//        song1.setAlbum(album);
-//        song1.save();
-//
-//        Song song2 = new Song();
-//        song2.setName("我爱的人");
-//        song2.setDuration(356);
-//        song2.setAlbum(album);
-//
-//        List<Song> songList = DataSupport.findAll(Song.class);
-//        System.out.println("Alum中的歌曲: "+ songList.toString());
+        /*Album album = new Album();
+        album.setName("我在江苏南京");
+        album.setPrice(10.09f);
+        //album.setCover("1,2,3,4,5".getBytes());
+        album.save();
+
+        Song song1 = new Song();
+        song1.setName("my love");
+        song1.setDuration(320);
+        song1.setAlbum(album);
+        song1.save();
+
+        Song song2 = new Song();
+        song2.setName("我爱的人");
+        song2.setDuration(356);
+        song2.setAlbum(album);
+        song2.save();
+
+        //查询
+        List<Song> songList = DataSupport.findAll(Song.class);
+        String str = "";
+        for (Song item : songList) {
+            str += item.toString() + "\n";
+        }
+        System.out.println("Alum中的歌曲: " + str);*/
+
+        // 按条件查询
+        List<Song> whereQry = DataSupport.where("name like ?", "%爱%").order("duration").find(Song.class);
+        String str = "";
+        for (Song item : whereQry) {
+            str += item.toString() + "\n";
+        }
+        System.out.println("按名字查询歌曲: \n" + str);
     }
 }
