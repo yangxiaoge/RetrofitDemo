bb1e71a2706d5826d9d3fbab1f0844a33362378d
yangxiaoge <364804051@qq.com>
2016-11-22 10:58:40 +0800

    dialog 弹出输入框

--- app/src/main/java/com/yjn/retrofitdemo/dialog_customer_layout/CustomerDialogActivity.java
+++ app/src/main/java/com/yjn/retrofitdemo/dialog_customer_layout/CustomerDialogActivity.java
@@ -1,9 +1,12 @@
 package com.yjn.retrofitdemo.dialog_customer_layout;
 
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,10 +32,11 @@ public class CustomerDialogActivity extends AppCompatActivity {
     private void showPasswordSetDailog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         final AlertDialog dialog = builder.create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
         View view = View.inflate(this, R.layout.dailog_set_smscode, null);
         // dialog.setView(view);// 将自定义的布局文件设置给dialog
-        dialog.setView(view, 0, 0, 0, 0);// 设置边距为0,保证在2.x的版本上运行没问题
+        dialog.setView(view, dpToPx(this,55), 0, dpToPx(this,55), 0);// 设置边距为0,保证在2.x的版本上运行没问题
 
         final EditText etPasswordConfirm = (EditText) view
                 .findViewById(R.id.sms_code);
@@ -61,5 +65,9 @@ public class CustomerDialogActivity extends AppCompatActivity {
 
         dialog.setCancelable(false);
         dialog.show();
+    }
+    public int dpToPx(Context context, int dp) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        return Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
     }
 }
